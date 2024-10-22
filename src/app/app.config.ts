dc8e94997a41ca1ba79a419757d2f08c9e59f23c
Matheussmaced <mateusbyakuya3@gmail.com>
2024-10-22 11:51:54 -0300

    feat: creating page table screen

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { TableScreenComponent } from './pages/table-screen/table-screen.component';
 
 const routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'table-screen', component: TableScreenComponent }
 ];
 
 export const appConfig: ApplicationConfig = {
---
+++ src/app/pages/table-screen/table-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-screen.component.html',
+  styleUrl: './table-screen.component.scss'
+})
+export class TableScreenComponent {
+
+}
