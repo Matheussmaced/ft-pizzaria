06e4bcc9f17be68c1f10f6d5d5c8b9919709fb3f
Matheussmaced <mateusbyakuya3@gmail.com>
2024-10-23 17:15:48 -0300

    refact: header-pages

---
+++ src/app/components/header-pages/header-pages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-pages',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-pages.component.html',
+  styleUrl: './header-pages.component.scss'
+})
+export class HeaderPagesComponent {
+
+}
--- src/app/pages/table-screen/table-screen.component.ts
+++ src/app/pages/table-screen/table-screen.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonsTableComponent } from '../../components/buttons-table/buttons-table.component';
 import { SideMenuComponent } from "../../components/side-menu/side-menu.component";
 import { CommonModule } from '@angular/common';
+import { HeaderPagesComponent } from '../../components/header-pages/header-pages.component';
 
 @Component({
   selector: 'app-table-screen',
   standalone: true,
-  imports: [ButtonsTableComponent, SideMenuComponent, CommonModule],
+  imports: [ButtonsTableComponent, SideMenuComponent, CommonModule, HeaderPagesComponent],
   templateUrl: './table-screen.component.html',
   styleUrl: './table-screen.component.scss'
 })
