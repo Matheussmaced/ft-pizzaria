c9e2c91b6c28e9f96ad6fc98dbc995c7a4f8d6ad
Matheussmaced <mateusbyakuya3@gmail.com>
2024-10-23 22:02:49 -0300

    feat: creating modal

--- src/app/components/header-pages/header-pages.component.ts
+++ src/app/components/header-pages/header-pages.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { AddTableDialogComponent } from '../add-table-dialog/add-table-dialog.component';
 
 @Component({
   selector: 'app-header-pages',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './header-pages.component.html',
   styleUrl: './header-pages.component.scss'
 })
 export class HeaderPagesComponent {
+  @Input() nameButton:string = "";
 
+  constructor(private dialog: MatDialog){}
+
+  openAddTableDialog():void{
+    const dialogRef = this.dialog.open(AddTableDialogComponent, {
+      width: '30rem',
+      panelClass: 'custom-dialog-container',
+      backdropClass: 'custom-backdrop',
+      position: {top: '-38rem', left: '40%' },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result?.success){
+        console.log("Mesa adicionada com sucesso");
+      }
+    })
+  }
 }
