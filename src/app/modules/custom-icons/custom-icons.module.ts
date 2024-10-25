daeac37af22dbc8c7558f76fade69603c6096480
Matheussmaced <mateusbyakuya3@gmail.com>
2024-10-25 18:55:52 -0300

    feat: creating total value container

--- src/app/modules/custom-icons/custom-icons.module.ts
+++ src/app/modules/custom-icons/custom-icons.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LucideAngularModule, Armchair, FileText, ScrollText, PackageSearch, CircleDollarSign, LogOut, CircleChevronDown, Trash2 } from 'lucide-angular';
+import { LucideAngularModule, Armchair, FileText, ScrollText, PackageSearch, CircleDollarSign, LogOut, Download, CircleChevronDown, Trash2 } from 'lucide-angular';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    LucideAngularModule.pick({Trash2, Armchair, FileText, ScrollText, PackageSearch, CircleDollarSign, LogOut, CircleChevronDown})
+    LucideAngularModule.pick({Download, Trash2, Armchair, FileText, ScrollText, PackageSearch, CircleDollarSign, LogOut, CircleChevronDown})
   ],
   exports: [
     LucideAngularModule
