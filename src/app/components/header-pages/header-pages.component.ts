a12c989ee6b332ba52a53ada4c8935e3e5940144
Matheussmaced <mateusbyakuya3@gmail.com>
2024-10-28 09:57:42 -0300

    fix: button header

--- src/app/components/header-pages/header-pages.component.ts
+++ src/app/components/header-pages/header-pages.component.ts
@@ -2,33 +2,17 @@ import { Component, Input } from '@angular/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { AddTableDialogComponent } from '../add-table-dialog/add-table-dialog.component';
 import { CommonModule } from '@angular/common';
+import { ButtonHeaderComponent } from "../button-header/button-header.component";
 
 @Component({
   selector: 'app-header-pages',
   standalone: true,
-  imports: [MatDialogModule, CommonModule],
+  imports: [MatDialogModule, CommonModule, ButtonHeaderComponent],
   templateUrl: './header-pages.component.html',
   styleUrl: './header-pages.component.scss'
 })
 export class HeaderPagesComponent {
-  @Input() nameButton:string = "";
   @Input() title:string = "";
+  @Input() nameButton:string = ""
   @Input() showButton:boolean = true;
-
-  constructor(private dialog: MatDialog){}
-
-  openAddTableDialog():void{
-    const dialogRef = this.dialog.open(AddTableDialogComponent, {
-      width: '30rem',
-      panelClass: 'custom-dialog-container',
-      backdropClass: 'custom-backdrop',
-      position: {top: '-38rem', left: '40%' },
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if(result?.success){
-        console.log("Mesa adicionada com sucesso");
-      }
-    })
-  }
 }
