3d19d437216b8e01045ca8259d7937ec6058d15f
Matheussmaced <mateusbyakuya3@gmail.com>
2024-11-08 12:11:59 -0300

    feat: creating call http for financial page

--- src/app/components/transaction-information/transaction-information.component.ts
+++ src/app/components/transaction-information/transaction-information.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CustomIconsModule } from '../../modules/custom-icons/custom-icons.module';
-
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FinancialService } from '../../services/financial.service';
+import { Financial } from '../../../model/financial/Financial';
+import { Transactions } from '../../../model/financial/Transactions';
 @Component({
   selector: 'app-transaction-information',
   standalone: true,
-  imports: [ CustomIconsModule ],
+  imports: [ CustomIconsModule, CommonModule, HttpClientModule],
   templateUrl: './transaction-information.component.html',
-  styleUrl: './transaction-information.component.scss'
+  styleUrl: './transaction-information.component.scss',
+  providers: [ FinancialService ]
 })
-export class TransactionInformationComponent {
+export class TransactionInformationComponent implements OnInit {
+  financials: Financial[] = [];
 
+  constructor ( private financialService: FinancialService ) {}
+
+  ngOnInit(): void {
+    this.financialService.getFinancial().subscribe((data: Financial[]) => {
+      this.financials = data.map(transactionHeader => ({
+        ...transactionHeader,
+        transactions: transactionHeader.transactions.map(transactionBody => ({
+          ...transactionBody,
+        }))
+      }))
+    });
+  }
 }
