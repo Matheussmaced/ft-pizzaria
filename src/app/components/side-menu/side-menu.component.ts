71f78e05a059d6f319a93016c9eb7f97ad5d7bb8
Matheussmaced <mateusbyakuya3@gmail.com>
2024-11-08 16:25:40 -0300

    feat: creating menu burguer

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,17 +1,44 @@
-import { Component } from '@angular/core';
-import { CustomIconsModule } from '../../modules/custom-icons/custom-icons.module';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { CustomIconsModule } from '../../modules/custom-icons/custom-icons.module';
 
 @Component({
   selector: 'app-side-menu',
-  standalone: true,
-  imports: [
-    CustomIconsModule,
-    RouterModule
-  ],
   templateUrl: './side-menu.component.html',
-  styleUrl: './side-menu.component.scss'
+  styleUrls: ['./side-menu.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule, CustomIconsModule]
 })
 export class SideMenuComponent {
+  menuOpen = false;
+  isDesktop = window.innerWidth > 980;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.isDesktop = window.innerWidth > 980;
+
+    if (this.isDesktop) {
+      this.menuOpen = true;
+    } else {
+      this.menuOpen = false;
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+
+    if (!this.isDesktop && this.menuOpen) {
+      const target = event.target as HTMLElement;
+      const isInsideMenu = target.closest('.main-container') || target.closest('.burguer-menu');
+
+      if (!isInsideMenu) {
+        this.menuOpen = false;
+      }
+    }
+  }
 }
--- src/app/modules/custom-icons/custom-icons.module.ts
+++ src/app/modules/custom-icons/custom-icons.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LucideAngularModule, Armchair, FileText, ScrollText, PackageSearch, CircleDollarSign, LogOut, Download, CircleChevronDown, Trash2, Pencil, CircleChevronUp, DollarSign } from 'lucide-angular';
+import { LucideAngularModule, Armchair, AlignLeft, FileText, ScrollText, PackageSearch, CircleDollarSign, LogOut, Download, CircleChevronDown, Trash2, Pencil, CircleChevronUp, DollarSign, CircleX } from 'lucide-angular';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    LucideAngularModule.pick({Pencil, Download, Trash2, Armchair, FileText, ScrollText, PackageSearch, CircleDollarSign, LogOut, CircleChevronDown, CircleChevronUp, DollarSign})
+    LucideAngularModule.pick({Pencil, Download, Trash2, Armchair, FileText, ScrollText, PackageSearch, CircleDollarSign, LogOut, CircleChevronDown, CircleChevronUp, DollarSign, AlignLeft, CircleX })
   ],
   exports: [
     LucideAngularModule
