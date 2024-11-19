8fbcfcae7c46413360c8df743895836a560925f0
Matheussmaced <mateusbyakuya3@gmail.com>
2024-11-19 20:54:25 -0300

    feat: printing form data

--- src/app/components/modal/adding-stock-product/adding-stock-product.component.ts
+++ src/app/components/modal/adding-stock-product/adding-stock-product.component.ts
@@ -1,16 +1,27 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-adding-stock-product',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './adding-stock-product.component.html',
   styleUrl: './adding-stock-product.component.scss'
 })
 export class AddingStockProductComponent {
   @Output() closeModal = new EventEmitter<void>();
 
+  formData = {
+    itemName: '',
+    quantity: 0,
+    unit: '',
+  };
+
   onCancel(){
     this.closeModal.emit();
+  }
+
+  onSubmit():void{
+    console.log('Informações do Formulario: ', this.formData)
   }
 }
--- src/app/components/modal/adding-table-modal/adding-table-modal.component.ts
+++ src/app/components/modal/adding-table-modal/adding-table-modal.component.ts
@@ -1,17 +1,26 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-adding-table-modal',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './adding-table-modal.component.html',
   styleUrl: './adding-table-modal.component.scss'
 })
 export class AddingTableModalComponent {
   @Output() closeModal = new EventEmitter<void>();
 
+  formData = {
+    numberTable: 0
+  };
+
   onCancel(){
     this.closeModal.emit();
+  }
+
+  onSubmit():void{
+    console.log('Dados do Formulario: ', this.formData)
   }
 }
